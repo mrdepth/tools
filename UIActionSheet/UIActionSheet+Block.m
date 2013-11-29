c89ced9cb5c8466479126f7fdfa1b2773ab44f0a
mrdepth <shimanski.artem@gmail.com>
2013-11-29 10:28:19 +0300

    Fixed action sheet

--- UIActionSheet/UIActionSheet+Block.m
+++ UIActionSheet/UIActionSheet+Block.m
@@ -113,7 +113,8 @@ destructiveButtonTitle:(NSString *)destructiveButtonTitle
 
 #pragma mark - UIActionSheetDelegate
 
-- (void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex {
+//- (void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex {
+- (void) actionSheet:(UIActionSheet *)actionSheet didDismissWithButtonIndex:(NSInteger)buttonIndex {
 	if (self.completionBlock) {
 		self.completionBlock(self, buttonIndex);
 		self.completionBlock = nil;
